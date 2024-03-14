b549aa66c31681a27aaad4b6cf27a73e4c752166
Victoria Gomez-Small <gomez-small.v@northeastern.edu>
2024-03-14 16:25:21 -0400

    updating the look of the navbar

--- src/Navigation/profile-Nav.js
+++ src/Navigation/profile-Nav.js
@@ -19,8 +19,9 @@ function Navigation({userId}) {
   // });
 
   const links = [
-    { to: "/home", label: "Home" },
-    { to: "/community", label: "Community" },
+    { path: "#/home", to: "home", label: "Home" },
+    { path: "#/community", to: "community", label: "Community" },
+    { path: "#/profile", to: "profile", label: "Profile" },
   ];
   const linksAtEnd = [{ to: "/profile", label: "Profile" }];
 
@@ -52,47 +53,66 @@ function Navigation({userId}) {
   useEffect(() => {
   }, [userId]);
 
+  // return (
+  //   <div className="d-flex justify-content-between p-2">
+  //     <div className="align-middle">
+  //       {links.map((link) => (
+  //         <Link
+  //           key={link.to}
+  //           to={link.to}
+  //           className={`pe-3 active`}
+  //           style={{ textDecoration: "none", color: "darkgreen" }}
+  //         >
+  //           {link.label}
+  //         </Link>
+  //       ))}
+  //     </div>
+  //     <div>
+  //       {linksAtEnd.map((link) => (
+  //         <Link
+  //           key={link.to}
+  //           to={link.to}
+  //           className={`pe-3 ${active(link.to)}`}
+  //           style={{ textDecoration: "none", color: "darkgreen" }}
+  //         >
+  //           {link.label}
+  //         </Link>
+  //       ))}
+  //       {/* shows sign in button if not logged in */}
+  //       {!userId && (
+  //         <Link
+  //           key={"/signin"}
+  //           to={"/signin"}
+  //           className="btn bg-success text-white"
+  //         >
+  //           Sign In
+  //         </Link>
+  //       )}
+  //       {userId && (
+  //         <Link onClick={signout} className="btn bg-secondary text-white">
+  //           Sign Out
+  //         </Link>
+  //       )}
+  //     </div>
+  //   </div>
+  // );
+
   return (
-    <div className="d-flex justify-content-between p-2">
-      <div className="align-middle">
-        {links.map((link) => (
-          <Link
-            key={link.to}
-            to={link.to}
-            className={`pe-3 ${active(link.to)}`}
-            style={{ textDecoration: "none", color: "darkgreen" }}
-          >
-            {link.label}
-          </Link>
-        ))}
-      </div>
-      <div>
-        {linksAtEnd.map((link) => (
-          <Link
-            key={link.to}
-            to={link.to}
-            className={`pe-3 ${active(link.to)}`}
-            style={{ textDecoration: "none", color: "darkgreen" }}
-          >
-            {link.label}
-          </Link>
-        ))}
-        {/* shows sign in button if not logged in */}
-        {!userId && (
-          <Link
-            key={"/signin"}
-            to={"/signin"}
-            className="btn bg-success text-white"
-          >
-            Sign In
-          </Link>
-        )}
-        {userId && (
-          <Link onClick={signout} className="btn bg-secondary text-white">
-            Sign Out
-          </Link>
-        )}
-      </div>
+    <div className=''>
+      <nav className="navbar navbar-expand-lg bg-transparent p-1" >
+        <a className="navbar-brand text-success" href="#">Trails</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav me-auto">
+            {links.map((link) => 
+             <a className={`nav-item nav-link text-success ${active(link.to)}`} href={link.path}>{link.label}</a>
+             )}
+          </div>
+          <button class="btn rounded-5 btn-success text-light" type="button">Sign Out</button>
+        </div>
+      </nav>
     </div>
   );
 }
